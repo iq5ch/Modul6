4316dfae20f7d84427f2958fb3c30f6926a87932
iq5ch <iqlima89chairunnisa@gmail.com>
2020-05-15 09:32:39 +0700

    Initial commit

---
+++ app/src/main/java/com/app/modul/Konfigurasi.java
@@ -0,0 +1,24 @@
+package com.app.modul;
+
+public class Konfigurasi {
+
+    public static final String URL_ADD = "http://192.168.43.22:8081/dbandroid/insert.php";
+    public static final String URL_GET_ALL = "http://192.168.43.22:8081/dbandroid/read.php";
+    public static final String URL_GET_MHS = "http://192.168.43.22:8081/dbandroid/select.php?id=";
+    public static final String URL_UPDATE_MHS = "http://192.168.43.22:8081/dbandroid/update.php";
+    public static final String URL_DELETE_MHS = "http://192.168.43.22:8081/dbandroid/delete.php?id=";
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+    public static final String MHS_ID="mhs_id";
+
+}
